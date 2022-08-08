6789ca6a5784e225499be4de2e15396175608c9b
Lesthy <Lesthy@gmail.com>
2022-08-08 14:54:51 +0300

    lest_code added

--- script.js
+++ script.js
@@ -90,3 +90,64 @@ function checkWin(currentClass) {
         })
     })
 }
+
+// lect_code , when game board is created with 9 a elements//
+let symbol = 'x';
+const board = document.querySelector('.board');
+const reset_btn = document.querySelector('.reset')
+
+board.onclick = function (event) {
+    if (event.target.tagName == 'DIV')
+        return;
+    let cell = event.target;
+    if (cell.textContent != '')
+        return;
+
+    cell.textContent = symbol;
+    if (checkWinner()) {
+        displayMessage('Winner is' + symbol + '!')
+    };
+    symbol = (symbol == 'x') ? 'o' : 'x';
+
+}
+
+reset_btn.onclick = function () {
+    for (let cell of board.chilcren) {
+        cell.textContent = '';
+        symbol = 'x';
+        displayMessage('');
+    }
+}
+
+const win_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function checkWinner() {
+
+    for (let cords of win_combinations) {
+        if (board.children[cords[0]].textContent == symbol &&
+            board.children[cords[1]].textContent == symbol &&
+            board.children[cords[2]].textContent == symbol
+        ) {
+            return true;
+        }
+    }
+    // board;
+    // symbol;
+    return true || false;
+}
+
+function displayMessage(message) {
+    document.querySelector('.message').textContent = message;
+}
+// lect_code  //
